16f2fae245b6e917b742e11ad4ba5e6b8f113325
yuriagopian <yuri.agopian@hotmail.com>
2023-01-18 09:47:50 -0300

    chore:  implements exception filter

--- src/common/filters/http-exception/http-exception.filter.ts
+++ src/common/filters/http-exception/http-exception.filter.ts
@@ -4,10 +4,25 @@ import {
   ExceptionFilter,
   HttpException,
 } from '@nestjs/common';
+import { Request, Response } from 'express';
 
 @Catch(HttpException)
 export class HttpExceptionFilter<T extends HttpException>
   implements ExceptionFilter
 {
-  catch(exception: T, host: ArgumentsHost) {}
+  catch(exception: T, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+
+    const status = exception.getStatus();
+    const exceptionResponse = exception.getResponse();
+
+    const errorType = typeof response;
+    if (errorType === 'string') {
+      return { message: exceptionResponse, status: response.status };
+    }
+
+    return exceptionResponse as object;
+  }
 }
