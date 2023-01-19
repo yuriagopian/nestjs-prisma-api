5d688d4c65b5ccaee85ca7c213a40369283203ca
yuriagopian <yuri.agopian@hotmail.com>
2023-01-19 10:07:18 -0300

    chore:  implements unauthorized exception filter

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { UnauthorizedInterceptor } from './common/errors/interceptors/unauthorized.interceptor';
+// import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,7 +15,9 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new HttpExceptionFilter());
+  // app.useGlobalFilters(new HttpExceptionFilter());
+
+  app.useGlobalInterceptors(new UnauthorizedInterceptor());
 
   await app.listen(process.env.PORT || 3000);
 }
