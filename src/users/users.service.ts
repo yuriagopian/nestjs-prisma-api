8ba7fa230dcbd6d7aca7371d73293295c64aa2ce
yuriagopian <yuri.agopian@hotmail.com>
2023-01-19 10:14:26 -0300

    chore:  implements unauthorized exception filter

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { NotFoundError } from 'src/common/errors/types/NotFoundError';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { UsersRepository } from './repositories/users.repository';
+import { UserEntity } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
@@ -15,8 +17,14 @@ export class UsersService {
     return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return this.repository.findOne(id);
+  async findOne(id: number): Promise<UserEntity> {
+    const user = await this.repository.findOne(id);
+
+    if (!user) {
+      throw new NotFoundError('Usuário não encontrado');
+    }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
