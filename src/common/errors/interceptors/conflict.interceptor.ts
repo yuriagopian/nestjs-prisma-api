3f70f91d86568fff763897b390d1426af4cd4acd
yuriagopian <yuri.agopian@hotmail.com>
2023-01-19 11:32:15 -0300

    chore:  implements database conflict error interceptor

--- src/common/errors/interceptors/conflict.interceptor.ts
+++ src/common/errors/interceptors/conflict.interceptor.ts
@@ -3,18 +3,21 @@ import {
   NestInterceptor,
   ExecutionContext,
   CallHandler,
+  ConflictException,
 } from '@nestjs/common';
 import { catchError, Observable } from 'rxjs';
-import { isPrismaError } from '../utils/is-prisma-error.util';
+import { ConflictError } from '../types/ConflictError';
 
 @Injectable()
-export class DatabaseInterceptor implements NestInterceptor {
+export class ConflictInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       catchError(error => {
-        if (isPrismaError(error)) {
+        if (error instanceof ConflictError) {
+          throw new ConflictException(error.message);
+        } else {
+          throw error;
         }
-        throw error;
       }),
     );
   }
