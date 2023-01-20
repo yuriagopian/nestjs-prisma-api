455a950c00ce82d55f16ab9ce6a4b45823c845bd
yuriagopian <yuri.agopian@hotmail.com>
2023-01-20 14:02:29 -0300

    chore:  implements posts repository

--- src/posts/repositories/posts.repository.ts
+++ src/posts/repositories/posts.repository.ts
@@ -1,7 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CreatePostDto } from '../dto/create-post.dto';
+import { UpdatePostDto } from '../dto/update-post.dto';
+import { PostEntity } from '../entities/post.entity';
 
 @Injectable()
 export class PostsRepository {
   constructor(private readonly prisma: PrismaService) {}
+
+  async create(createPostDto: CreatePostDto): Promise<PostEntity> {
+    return this.prisma.post.create({
+      data: createPostDto,
+    });
+  }
+
+  async findAll(): Promise<PostEntity[]> {
+    return this.prisma.post.findMany();
+  }
+
+  async findOne(id: number): Promise<PostEntity> {
+    return this.prisma.post.findUnique({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async update(id: number, updateUserDto: UpdatePostDto): Promise<PostEntity> {
+    return this.prisma.post.update({
+      where: {
+        id,
+      },
+      data: updateUserDto,
+    });
+  }
+
+  async remove(id: number): Promise<PostEntity> {
+    return this.prisma.post.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
