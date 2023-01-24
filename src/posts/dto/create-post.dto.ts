85492db8af5262ecfa8710817e36b33e2aed225e
yuriagopian <yuri.agopian@hotmail.com>
2023-01-24 10:05:04 -0300

    chore:  implements api documentation with swagger

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,14 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreatePostDto {
+  @ApiProperty({
+    description: 'Titulo da publicação',
+    required: true,
+  })
   @IsString()
   @IsNotEmpty()
   title: string;
 
+  @ApiProperty({
+    description: 'Conteúdo  da publicação',
+  })
   @IsString()
   @IsOptional()
   content: string;
 
+  @ApiProperty({
+    description: 'Email do author da publicação',
+  })
   @IsNotEmpty()
   @IsEmail()
   authorEmail: string;
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,14 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty({
+    description: 'Email do usuário',
+  })
   @IsEmail()
   @IsNotEmpty()
   email: string;
 
+  @ApiProperty({
+    description: 'Nome completo do usuário do usuário',
+  })
   @IsString()
   @IsNotEmpty()
   name: string;
 
+  @ApiProperty({
+    description: 'Define se o usuário é administrador',
+    default: false,
+  })
   @IsBoolean()
   @IsNotEmpty()
   admin: boolean;
